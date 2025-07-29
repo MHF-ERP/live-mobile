20fa8dccd854501a71f2f2738341f9000e4af676
fahd-74 <fhakem75@gmail.com>
2025-07-29 21:33:02 +0300

    notification

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { registerForPushNotificationsAsync } from "@/utils/notification";
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
+import * as Notifications from "expo-notifications";
 import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useRef } from "react";
 import "react-native-reanimated";
 
 export default function RootLayout() {
@@ -15,6 +18,35 @@ export default function RootLayout() {
     "Tajawal-Regular": require("../assets/fonts/Tajawal-Regular.ttf"),
     "Tajawal-Bold": require("../assets/fonts/Tajawal-Bold.ttf"),
   });
+
+  const notificationListener = useRef<Notifications.Subscription | null>(null);
+  const responseListener = useRef<Notifications.Subscription | null>(null);
+
+  useEffect(() => {
+    registerForPushNotificationsAsync();
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      if (notificationListener.current) {
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        );
+      }
+      if (responseListener.current) {
+        Notifications.removeNotificationSubscription(responseListener.current);
+      }
+    };
+  }, []);
+
   if (!loaded) return null;
 
   return (
